82921e0c84b5a066250fdb7e2fef2b5c1e176512
Scott Christensen <mscottchristensen@gmail.com>
2019-12-02 21:46:53 -0500

    set up ch4 program to accept arguments to enable ch4 exercises

--- ch4/test.c
+++ ch4/test.c
@@ -2,15 +2,69 @@
 #include <stdlib.h>
 #include <string.h>
 #include <inttypes.h>
+#include <ctype.h>
+#include <getopt.h>
 
 #include "loader.h"
 
+static void print_help(void);
+
 int main(int argc, char * argv[])
 {
+	unsigned char s_flag = 0U, d_flag = 0U, f_flag = 0U, x_flag = 0U;
+	char* x_secname = NULL;
 	unsigned int i;
+	int c, index;
 	Binary * bin = NULL;
-	char * filename = NULL;
+	char * filename = NULL;	
 
+	struct option long_options[] = 
+	{
+		{"func_symbols", 	no_argument, 		NULL, 	'f'},
+		{"important_sections", 	no_argument, 		NULL, 	's'},
+		{"data_symbols", 	no_argument, 		NULL, 	'd'},
+		{"hex_dump", 		required_argument, 	NULL, 	'x'},
+		{"h", 			no_argument, 		NULL, 	'h'},
+		{"help",		no_argument,		NULL, 	'h'},
+		{0, 0, 0, 0}
+	};
+
+	while ((c = getopt_long(argc, argv, "sdfx:h", long_options, &index)) != -1)
+	{
+    		switch (c)
+		{
+			case 's':
+				s_flag = 1;
+				break;
+			case 'd':
+				d_flag = 1;
+				break;
+			case 'f':
+				f_flag = 1;
+				break;
+			case 'x':
+				x_flag = 1;
+				x_secname = optarg;
+				break;
+			case 'h':
+				print_help();
+				return 0;
+			case '?':
+				print_help();
+				return 1;
+			default:
+				abort();	
+		}
+	}
+
+	printf ("s_flag = %d, d_flag = %d, f_flag = %d, x_flag = %d, x_secname = %s\n", s_flag, d_flag, f_flag, x_flag, x_secname);
+
+  	for (index = optind; index < argc; index++)
+		printf ("Non-option argument %s\n", argv[index]);
+
+	printf("optind = %d, argc = %d\n", optind, argc);
+
+  	return 0;
 	if(argc == 2)
 	{
 		/* allocate a buffer for filename */
@@ -65,4 +119,15 @@ int main(int argc, char * argv[])
 	}
 
 	return 0;
+}
+
+static void print_help(void)
+{
+	printf("Usage: loader [options] binary\n");
+	printf("Options:\n");
+	printf("\t%-40s %s\n", "-f (--func_symbols)", "display function symbols");
+	printf("\t%-40s %s\n", "-d (--data_symbols)", "display data symbols");
+	printf("\t%-40s %s\n", "-s (--important_sections)", "display code and data sections");
+	printf("\t%-40s %s\n", "-x (--hex_dump) <.section_name>", "display hex dump of section passed as argument");
+	printf("\t%-40s %s\n\n", "-h (--h or --help)", "display this information and return with no action"); 
 }
